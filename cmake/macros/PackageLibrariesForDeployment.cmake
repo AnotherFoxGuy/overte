715d776af10f6f7277ebb7bf780930a0f3d68344
Edgar <Edgar@AnotherFoxGuy.com>
2024-02-28 15:42:17 +0100

    more fix

--- cmake/macros/PackageLibrariesForDeployment.cmake
+++ cmake/macros/PackageLibrariesForDeployment.cmake
@@ -10,37 +10,39 @@
 #
 
 macro(PACKAGE_LIBRARIES_FOR_DEPLOYMENT)
-    if (WIN32)
-        set(PLUGIN_PATH "plugins")
+    if (NOT SONAR_SCAN)
+        if (WIN32)
+            set(PLUGIN_PATH "plugins")
 
-        get_target_property(Qt_Core_Location Qt5::Core LOCATION)
-        get_filename_component(QT_BIN_DIR ${Qt_Core_Location} DIRECTORY)
-        find_program(WINDEPLOYQT_COMMAND windeployqt PATHS QT_BIN_DIR)
+            get_target_property(Qt_Core_Location Qt5::Core LOCATION)
+            get_filename_component(QT_BIN_DIR ${Qt_Core_Location} DIRECTORY)
+            find_program(WINDEPLOYQT_COMMAND windeployqt PATHS QT_BIN_DIR)
 
-        if (NOT WINDEPLOYQT_COMMAND)
-            message(FATAL_ERROR "Could not find windeployqt at ${QT_DIR}/bin. windeployqt is required.")
+            if (NOT WINDEPLOYQT_COMMAND)
+                message(FATAL_ERROR "Could not find windeployqt at ${QT_DIR}/bin. windeployqt is required.")
+            endif ()
+
+            # add a post-build command to call windeployqt to copy Qt plugins
+            set(CMD "${WINDEPLOYQT_COMMAND} ${EXTRA_DEPLOY_OPTIONS} --no-compiler-runtime --no-opengl-sw --no-angle -no-system-d3d-compiler")
+
+            file(WRITE "${CMAKE_CURRENT_BINARY_DIR}/windeploy-${TARGET_NAME}.bat" "${CMD} %*")
+            add_custom_command(
+                    TARGET ${TARGET_NAME}
+                    POST_BUILD
+                    COMMAND "${CMAKE_CURRENT_BINARY_DIR}/windeploy-${TARGET_NAME}.bat" $<$<OR:$<CONFIG:Release>,$<CONFIG:MinSizeRel>,$<CONFIG:RelWithDebInfo>>:--release> \"$<TARGET_FILE:${TARGET_NAME}>\"
+            )
+
         endif ()
 
-        # add a post-build command to call windeployqt to copy Qt plugins
-        set(CMD "${WINDEPLOYQT_COMMAND} ${EXTRA_DEPLOY_OPTIONS} --no-compiler-runtime --no-opengl-sw --no-angle -no-system-d3d-compiler")
-
-        file(WRITE "${CMAKE_CURRENT_BINARY_DIR}/windeploy-${TARGET_NAME}.bat" "${CMD} %*")
+        # Add a post-build command to copy the libraries beside the executable
         add_custom_command(
                 TARGET ${TARGET_NAME}
                 POST_BUILD
-                COMMAND "${CMAKE_CURRENT_BINARY_DIR}/windeploy-${TARGET_NAME}.bat" $<$<OR:$<CONFIG:Release>,$<CONFIG:MinSizeRel>,$<CONFIG:RelWithDebInfo>>:--release> \"$<TARGET_FILE:${TARGET_NAME}>\"
+                COMMAND ${CMAKE_COMMAND}
+                -DBUNDLE_EXECUTABLE="$<TARGET_FILE:${TARGET_NAME}>"
+                -DBUNDLE_PLUGIN_DIR="$<TARGET_FILE_DIR:${TARGET_NAME}>/${PLUGIN_PATH}"
+                -DLIB_PATHS="${CMAKE_BINARY_DIR}/conanlibs/$<CONFIGURATION>"
+                -P "${CMAKE_SOURCE_DIR}/cmake/FixupBundlePostBuild.cmake"
         )
-
     endif ()
-
-    # Add a post-build command to copy the libraries beside the executable
-    add_custom_command(
-            TARGET ${TARGET_NAME}
-            POST_BUILD
-            COMMAND ${CMAKE_COMMAND}
-            -DBUNDLE_EXECUTABLE="$<TARGET_FILE:${TARGET_NAME}>"
-            -DBUNDLE_PLUGIN_DIR="$<TARGET_FILE_DIR:${TARGET_NAME}>/${PLUGIN_PATH}"
-            -DLIB_PATHS="${CMAKE_BINARY_DIR}/conanlibs/$<CONFIGURATION>"
-            -P "${CMAKE_SOURCE_DIR}/cmake/FixupBundlePostBuild.cmake"
-    )
 endmacro()
